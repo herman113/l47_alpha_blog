054177238b7aa50c673a78451415e4c0cc16a77e
herman_soto <hermansoto@me.com>
2016-04-13 01:03:10 +0000

    create article table, model and add validations

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }  # this will only allow data to hit table if a title is present and the lenth is within range
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
