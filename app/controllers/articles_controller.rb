0f75b79a7a2c51094f594b8e605d1a77f85837f2
herman_soto <hermansoto@me.com>
2016-05-11 23:28:11 +0000

    added message partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)#white list means remove block for title and description to go from browser to console?.who the hell blocks it!
    
     if @article.save
-    flash[:notice] = "Article was successfully created"   #flash[:notice] what is this a hash or sytanx? I'm Lost!
+    flash[:success] = "Article was successfully created"   #flash[:notice] what is this a hash or sytanx? I'm Lost!
     redirect_to article_path(@article) ##I dont understand this argument.
     else
       render 'new' #render :new  #what is the difference between render and redirect_to?
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
   def update
    # @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
   def destroy
    # @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
  
