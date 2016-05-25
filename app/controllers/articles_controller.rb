708f3a279e43745734867e54bc813c03840230db
herman_soto <hermansoto@me.com>
2016-05-25 02:27:22 +0000

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -66,7 +66,7 @@ class ArticlesController < ApplicationController
     end
     
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only edit or delete your own articles"
         redirect_to root_path#how can i redirect to current user show page?
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  #has_many :articles
+  has_many :articles, dependent: :destroy
   before_save {self.email = email.downcase }
   #validates :username, presence: true, uniqueness: true, #This has no case sensitive
   validates  :username, presence: true,
