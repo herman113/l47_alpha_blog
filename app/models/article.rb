f2e83eae172d286383d8f693ec2b244b158a4c26
herman_soto <hermansoto@me.com>
2016-05-25 22:30:32 +0000

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   belongs_to :user
   validates :user_id, presence: true
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }  # this will only allow data to hit table if a title is present and the lenth is within range
